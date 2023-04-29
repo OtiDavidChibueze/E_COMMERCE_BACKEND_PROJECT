598fce3f8e715d36bee02b1ba56ca11b9d983ef2
OtiDavidChibueze <109368512+OtiDavidChibueze@users.noreply.github.com>
2023-04-29 01:44:39 +0100

    updated keys

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,3 +1,4 @@
+//* E_COMMERCE KEYS
 require('dotenv').config()
 
 module.exports = {
@@ -5,6 +6,7 @@ module.exports = {
   SERVER_PORT: process.env.SERVER_PORT,
   SECRET: process.env.SECRET,
   ADMIN_SECRET: process.env.ADMIN_SECRET,
+  SUPER_ADMIN_SECRET: process.env.SUPER_ADMIN_SECRET,
   MAIL: process.env.MAIL,
   MAIL_P: process.env.MAIL_P,
 }
