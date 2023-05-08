9164e7b1b8059cc52f55e5643bc1f4c5b8417fc3
OtiDavidChibueze <109368512+OtiDavidChibueze@users.noreply.github.com>
2023-05-08 08:54:19 +0100

    created user routes

---
+++ src/router/user.js
@@ -0,0 +1,34 @@
+//* USER ROUTER
+const router = require('express').Router()
+const controller = require('../controllers/user')
+const { AdminAuthToken } = require('../middleware/AdminAuthentication')
+const { UserAuthToken } = require('../middleware/UserAuthentication')
+
+//* USER URL ROUTE
+router.get('/', AdminAuthToken, controller.get_users)
+
+router.get('/counts', AdminAuthToken, controller.get_users_counts)
+
+router.put('/changePassword', UserAuthToken, controller.changeUserPassword)
+
+router.get('/:userId', AdminAuthToken, controller.get_users_by_id)
+
+router.post('/login', controller.post_login)
+
+router.post('/register', controller.post_register)
+
+router.post('/logout', UserAuthToken, controller.post_logOut)
+
+router.put('/:userId', UserAuthToken, controller.put)
+
+router.put('/blockUser/:userId', AdminAuthToken, controller.blockUserById)
+
+router.put('/unblockUser/:userId', AdminAuthToken, controller.unBlockUserById)
+
+router.post('/forgotPassword', controller.forgottenPassword)
+
+router.put('/resetToken/:token', controller.resetToken)
+
+router.delete('/:userId', AdminAuthToken, controller.delete_user)
+
+module.exports = router
