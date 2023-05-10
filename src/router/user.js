582e8dbf966af2ff7e14e6ec4f4bdb09962f1e89
OtiDavidChibueze <109368512+OtiDavidChibueze@users.noreply.github.com>
2023-05-10 10:17:49 +0100

    updated user router

--- src/router/user.js
+++ src/router/user.js
@@ -11,7 +11,11 @@ router.get(
   controller.get_All_Users_And_Also_Search_Users_With_Emails
 )
 
+router.get('/getCart', UserAuthToken, controller.getUserCart)
+
 router.get('/counts', AdminAuthToken, controller.get_users_counts)
+
+router.get('/wishList', UserAuthToken, controller.getWishList)
 
 router.put('/changePassword', UserAuthToken, controller.changeUserPassword)
 
@@ -20,6 +24,8 @@ router.get('/:userId', AdminAuthToken, controller.get_users_by_id)
 router.put('/login', controller.post_login)
 
 router.post('/register', controller.post_register)
+
+router.post('/applyDiscount', UserAuthToken, controller.applyDiscount)
 
 router.put('/logout', UserAuthToken, controller.post_logOut)
 
@@ -31,8 +37,14 @@ router.put('/unblockUser/:userId', AdminAuthToken, controller.unBlockUserById)
 
 router.post('/forgotPassword', controller.forgottenPassword)
 
+router.delete('/emptyCart', UserAuthToken, controller.emptyCart)
+
 router.put('/resetToken/:token', controller.resetToken)
 
 router.delete('/:userId', AdminAuthToken, controller.delete_user)
+
+router.put('/add/Address', UserAuthToken, controller.add_Address)
+
+router.post('/addToCart', UserAuthToken, controller.addToCart)
 
 module.exports = router
