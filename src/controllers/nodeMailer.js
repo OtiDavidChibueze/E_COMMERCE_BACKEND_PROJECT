e146dc7adbc7e2ac27c16da7bc625d6efebb7c2f
OtiDavidChibueze <109368512+OtiDavidChibueze@users.noreply.github.com>
2023-05-12 16:27:56 +0100

    created eshop nodemailer

---
+++ src/controllers/nodeMailer.js
@@ -0,0 +1,35 @@
+//* E_COMMERCE NODE MAILER
+const nodemailer = require('nodemailer')
+const keys = require('../config/keys')
+
+//* NODE MAILER FUNCTION
+const sendEmail = async (data, req, res) => {
+  let transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false, //* true for 465, false for other ports
+    auth: {
+      user: keys.MAIL, //* generated ethereal user
+      pass: keys.MAIL_P, //* generated ethereal password
+    },
+  })
+
+  //* send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"hello 👻" <funds@example.com>', //* sender address
+    to: data.to, //* list of receivers
+    subject: data.subject, //* Subject line
+    text: data.text, //* plain text body
+    html: data.htm, //* html body
+  })
+
+  console.log('Message sent: %s', info.messageId)
+  //* Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  //* Preview only available when sending through an Ethereal account
+  console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info))
+  //* Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+}
+
+//* EXPORT THE EMAIL
+module.exports = sendEmail
