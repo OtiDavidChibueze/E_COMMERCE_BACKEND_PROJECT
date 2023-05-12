86e83db787722819a4723f3a2d5a5583de067558
OtiDavidChibueze <109368512+OtiDavidChibueze@users.noreply.github.com>
2023-05-12 19:28:28 +0100

    updated admin router

--- src/router/isAdmin.js
+++ src/router/isAdmin.js
@@ -1,40 +1,66 @@
-//* IS ADMIN ROUTER
+//* ADMIN ROUTER
 const router = require('express').Router()
 const controller = require('../controllers/isAdmin')
-const { AdminAuthToken } = require('../middleware/AdminAuthentication')
 const { SuperAdminAuth } = require('../middleware/SuperAdminAuthentication')
+const { AdminAuthToken } = require('../middleware/AdminAuthentication')
 
-//* IS ADMIN ROUTES
-router.get('/', SuperAdminAuth, controller.getAdminsOrSearchForAdminsUsingEmail)
+//* ADMIN URL ROUTE
+router.get(
+  '/',
+  SuperAdminAuth,
+  controller.get_All_Admins_And_Also_Search_Admins_With_Emails
+)
 
-router.get('/counts', SuperAdminAuth, controller.get_Admins_Counts)
+router.get('/active/counts', SuperAdminAuth, controller.getActiveAdminsCounts)
 
-router.get('/active/on', SuperAdminAuth, controller.get_Admins_Active)
+router.get('/offline/counts', SuperAdminAuth, controller.getOfflineAdminsCounts)
 
-router.get('/active/off', SuperAdminAuth, controller.get_Admins_non_Active)
+router.get('/active', SuperAdminAuth, controller.getActiveAdmins)
 
-router.post('/register', SuperAdminAuth, controller.register_Admins)
+router.get('/offline', SuperAdminAuth, controller.getOfflineAdmins)
 
-router.put('/changePassword', AdminAuthToken, controller.changePassword)
+router.get('/getOrder', SuperAdminAuth, controller.getOrder)
 
-router.put('/login', controller.login_Admin)
+router.get('/getCart', AdminAuthToken, controller.getAdminCart)
 
-router.put('/updateAdmin', AdminAuthToken, controller.updateAdmin)
+router.get('/counts', SuperAdminAuth, controller.get_Admins_counts)
 
-router.post('/logout', AdminAuthToken, controller.logOut_Admin)
+router.post('/addToWishList', AdminAuthToken, controller.addToWishList)
 
-router.get('/:adminId', SuperAdminAuth, controller.get_Admins_By_Id)
+router.get('/wishList', AdminAuthToken, controller.getWishList)
 
-router.put('/:adminId', SuperAdminAuth, controller.update_Admins_By_Id)
+router.put('/changePassword', AdminAuthToken, controller.changeAdminPassword)
+
+router.get('/:adminId', SuperAdminAuth, controller.get_Admins_by_id)
+
+router.put('/update', AdminAuthToken, controller.updateAdmin)
+
+router.post('/login', controller.post_login)
+
+router.post('/register', SuperAdminAuth, controller.post_register)
+
+router.post('/applyDiscount', AdminAuthToken, controller.applyDiscount)
+
+router.post('/logout', AdminAuthToken, controller.post_logOut)
+
+router.put('/:adminId', SuperAdminAuth, controller.put_update_Admin_By_Id)
+
+router.put('/blockUser/:adminId', SuperAdminAuth, controller.blockAdminById)
+
+router.put('/unblockUser/:adminId', SuperAdminAuth, controller.unBlockAdminById)
+
+router.post('/forgotPassword', controller.forgottenPassword)
+
+router.delete('/emptyCart', AdminAuthToken, controller.emptyCart)
+
+router.put('/resetToken/:token', controller.resetToken)
 
 router.delete('/:adminId', SuperAdminAuth, controller.delete_Admin)
 
-router.post('/forgottenPassword', controller.forgottenPassword)
+router.put('/add/Address', AdminAuthToken, controller.add_Address)
 
-router.put('/resetToken/:tokenId', controller.resetToken)
+router.post('/addToCart', AdminAuthToken, controller.addToCart)
 
-router.put('/block/:adminId', SuperAdminAuth, controller.blockAdmin)
-
-router.put('/unblock/:adminId', SuperAdminAuth, controller.unBlockAdmin)
+router.post('/createOrder', AdminAuthToken, controller.createOrder)
 
 module.exports = router
