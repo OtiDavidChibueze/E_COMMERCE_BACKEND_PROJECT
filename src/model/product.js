83db1e48b048e64a726acd2546f797acab8623a5
OtiDavidChibueze <109368512+OtiDavidChibueze@users.noreply.github.com>
2023-05-12 16:33:48 +0100

    created product model

---
+++ src/model/product.js
@@ -0,0 +1,102 @@
+//* PRODUCT MODEL
+const mongoose = require('mongoose')
+const mongoosePaginate = require('mongoose-paginate-v2')
+const Schema = mongoose.Schema
+
+const ProductSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
+    },
+    slug: {
+      type: String,
+      unique: true,
+      lowercase: true,
+    },
+    brand: {
+      type: String,
+      ref: 'Brand',
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    countInStock: {
+      type: Number,
+      required: true,
+    },
+    sold: {
+      type: Number,
+      default: 0,
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    images: [],
+    category: {
+      type: String,
+      ref: 'Category',
+      required: true,
+    },
+    isFeatured: {
+      type: Boolean,
+      default: false,
+    },
+    numberReviews: {
+      type: Number,
+      default: 0,
+    },
+    Like: {
+      type: Boolean,
+      default: false,
+    },
+    DisLike: {
+      type: Boolean,
+      default: false,
+    },
+    LikedUsers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+    DisLikedUsers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+    ratings: [
+      {
+        comment: {
+          type: String,
+        },
+        star: {
+          type: Number,
+        },
+        postedBy: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'User',
+        },
+      },
+    ],
+    totalRating: {
+      type: String,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+)
+
+ProductSchema.plugin(mongoosePaginate)
+
+const ProductModel = mongoose.model('Product', ProductSchema)
+
+module.exports = ProductModel
