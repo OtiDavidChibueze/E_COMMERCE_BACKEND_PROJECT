e24ca92d71254862c8ba32afca70e51c7d6c305f
OtiDavidChibueze <109368512+OtiDavidChibueze@users.noreply.github.com>
2023-05-15 14:56:53 +0100

    updated user router

--- src/router/user.js
+++ src/router/user.js
@@ -1,58 +1,62 @@
 //* USER ROUTER
 const router = require('express').Router()
 const controller = require('../controllers/user')
-const { AdminAuthToken } = require('../middleware/AdminAuthentication')
-const { UserAuthToken } = require('../middleware/UserAuthentication')
+
+const { authorization } = require('../middleware/authorization')
 
 //* USER URL ROUTE
 router.get(
   '/',
-  AdminAuthToken,
+  authorization,
   controller.get_All_Users_And_Also_Search_Users_With_Emails
 )
 
-router.get('/getOrder', UserAuthToken, controller.getOrder)
+router.get('/active/counts', authorization, controller.getActiveUsersCounts)
 
-router.get('/getCart', UserAuthToken, controller.getUserCart)
+router.get('/offline/counts', authorization, controller.getOfflineUsersCounts)
 
-router.get('/counts', AdminAuthToken, controller.get_users_counts)
+router.get('/getOrder', authorization, controller.getOrder)
 
-router.post('/addToWishList', UserAuthToken, controller.addToWishList)
+router.post('/addToWishList', authorization, controller.addToWishList)
 
-router.get('/wishList', UserAuthToken, controller.getWishList)
+router.get('/getCart', authorization, controller.getUserCart)
 
-router.put('/changePassword', UserAuthToken, controller.changeUserPassword)
+router.get('/counts', authorization, controller.get_users_counts)
 
-router.get('/:userId', AdminAuthToken, controller.get_users_by_id)
+router.get('/wishList', authorization, controller.getWishList)
 
-router.put('/updateUser', UserAuthToken, controller.updateUser)
+router.put('/changePassword', authorization, controller.changeUserPassword)
+
+router.get('/:userId', authorization, controller.get_users_by_id)
+
+router.put('/updateUser', authorization, controller.updateUser)
 
 router.post('/login', controller.post_login)
 
 router.post('/register', controller.post_register)
 
-router.post('/applyDiscount', UserAuthToken, controller.applyDiscount)
+router.post('/applyDiscount', authorization, controller.applyDiscount)
 
-router.post('/logout', UserAuthToken, controller.post_logOut)
+router.post('/logout', authorization, controller.post_logOut)
 
-router.put('/:userId', AdminAuthToken, controller.put_update_user_By_Id)
+router.put('/:userId', authorization, controller.put_update_user_By_Id)
 
-router.put('/blockUser/:userId', AdminAuthToken, controller.blockUserById)
+router.put('/blockUser/:userId', authorization, controller.blockUserById)
 
-router.put('/unblockUser/:userId', AdminAuthToken, controller.unBlockUserById)
+router.put('/unblockUser/:userId', authorization, controller.unBlockUserById)
 
 router.post('/forgotPassword', controller.forgottenPassword)
 
-router.delete('/emptyCart', UserAuthToken, controller.emptyCart)
+router.delete('/emptyCart', authorization, controller.emptyCart)
 
 router.put('/resetToken/:token', controller.resetToken)
 
-router.delete('/:userId', AdminAuthToken, controller.delete_user)
+router.delete('/:userId', authorization, controller.delete_user)
 
-router.put('/add/Address', UserAuthToken, controller.add_Address)
+router.put('/add/Address', authorization, controller.add_Address)
 
-router.post('/addToCart', UserAuthToken, controller.addToCart)
+router.post('/addToCart', authorization, controller.addToCart)
 
-router.post('/createOrder', UserAuthToken, controller.createOrder)
+router.post('/createOrder', authorization, controller.createOrder)
 
 module.exports = router
