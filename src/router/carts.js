a850cc86316789261ddc9ed35a5305b0eae4024a
OtiDavidChibueze <109368512+OtiDavidChibueze@users.noreply.github.com>
2023-05-15 14:55:05 +0100

    updated cart router

--- src/router/carts.js
+++ src/router/carts.js
@@ -1,12 +1,12 @@
 //* CART ROUTER
 const router = require('express').Router()
 const controller = require('../controllers/cart')
-const { AdminAuthToken } = require('../middleware/AdminAuthentication')
+const { authorization } = require('../middleware/authorization')
 
 //* CART ROUTERS
 
-router.get('/', AdminAuthToken, controller.getCarts)
+router.get('/', authorization, controller.getCarts)
 
-router.get('/:id', AdminAuthToken, controller.getCartById)
+router.get('/:id', authorization, controller.getCartById)
 
 module.exports = router
