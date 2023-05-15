c78d9d5ec9919cba9fee68db772cf64519b4d1a3
OtiDavidChibueze <109368512+OtiDavidChibueze@users.noreply.github.com>
2023-05-15 14:56:18 +0100

    updated order router

--- src/router/order.js
+++ src/router/order.js
@@ -1,19 +1,19 @@
 //* ORDER ROUTE
 const router = require('express').Router()
 const controller = require('../controllers/order')
-const { AdminAuthToken } = require('../middleware/AdminAuthentication')
+const { authorization } = require('../middleware/authorization')
 
 //* ORDER ROUTES
 
-router.get('/', AdminAuthToken, controller.getOrders)
+router.get('/', authorization, controller.getOrders)
 
-router.get('/counts', AdminAuthToken, controller.getOrdersCounts)
+router.get('/counts', authorization, controller.getOrdersCounts)
 
-router.get('/:id', AdminAuthToken, controller.getOrderById)
+router.get('/:id', authorization, controller.getOrderById)
 
 router.put(
   '/updateStatus/:orderId',
-  AdminAuthToken,
+  authorization,
   controller.updateUserOrderStatus
 )
 
