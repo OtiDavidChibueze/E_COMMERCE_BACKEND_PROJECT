39b49b2bfa53b7b85da61c05b5d4b7b44af2a77a
OtiDavidChibueze <109368512+OtiDavidChibueze@users.noreply.github.com>
2023-05-15 14:55:43 +0100

    updated coupon router

--- src/router/coupon.js
+++ src/router/coupon.js
@@ -2,19 +2,19 @@
 const express = require('express')
 const router = express.Router()
 const controller = require('../controllers/coupon')
-const { AdminAuthToken } = require('../middleware/AdminAuthentication')
+const { authorization } = require('../middleware/authorization')
 
 //*  ROUTES
-router.get('/', AdminAuthToken, controller.get)
+router.get('/', authorization, controller.get)
 
-router.get('/counts', AdminAuthToken, controller.getCouponCounts)
+router.get('/counts', authorization, controller.getCouponCounts)
 
-router.post('/create', AdminAuthToken, controller.post)
+router.post('/create', authorization, controller.post)
 
-router.get('/:couponId', AdminAuthToken, controller.getCouponById)
+router.get('/:couponId', authorization, controller.getCouponById)
 
-router.put('/:couponId', AdminAuthToken, controller.put)
+router.put('/:couponId', authorization, controller.put)
 
-router.delete('/:couponId', AdminAuthToken, controller.delete)
+router.delete('/:couponId', authorization, controller.delete)
 
 module.exports = router
